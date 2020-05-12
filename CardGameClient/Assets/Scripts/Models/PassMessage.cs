8c940f4de53974db54165c8c0fccfb96d8183c76
connellyj <juliakconnelly@gmail.com>
2020-05-12 14:38:20 -0500

    hearts is working

---
+++ CardGameClient/Assets/Scripts/Models/PassMessage.cs
@@ -0,0 +1,31 @@
+﻿public class PassMessage : Message
+{
+    public string PassingPlayer;
+    public string PassingTo;
+    public int NumToPass;
+    public Card[] PassedCards;
+    public string Type;
+
+    public PassMessage(string player, int numToPass=0, string passingTo = "", Card[] cards = null) : base("Pass")
+    {
+        PassingPlayer = player;
+        PassingTo = passingTo;
+        NumToPass = numToPass;
+        PassedCards = cards;
+    }
+
+    public override bool IsValid()
+    {
+        return Type == "Pass";
+    }
+
+    public override string GenerateId()
+    {
+        return PassingPlayer + "to" + PassingTo;
+    }
+
+    protected override void SetType(string type)
+    {
+        Type = type;
+    }
+}
