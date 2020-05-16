ad427b98d3be4eb0570edc5b7aa9265f3b478ac9
connellyj <juliakconnelly@gmail.com>
2020-05-16 01:30:22 -0500

    restart

---
+++ CardGameClient/Assets/Scripts/ExitButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ExitButton : MonoBehaviour
+{
+    void Start()
+    {
+        gameObject.GetComponent<Button>().onClick.AddListener(Application.Quit);
+    }
+}
---
+++ CardGameClient/Assets/Scripts/Models/RestartMessage.cs
@@ -0,0 +1,20 @@
+﻿public class RestartMessage : Message
+{
+    public string PlayerName;
+    public string Type;
+
+    public RestartMessage(string playerName) : base("Restart")
+    {
+        PlayerName = playerName;
+    }
+
+    public override bool IsValid()
+    {
+        return Type == "Restart";
+    }
+
+    protected override void SetType(string type)
+    {
+        Type = type;
+    }
+}
